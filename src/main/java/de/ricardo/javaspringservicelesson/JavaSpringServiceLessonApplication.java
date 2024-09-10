5c3292a35de6e7695834db1fe7d30e05e5e981a1
Ricardo Lemser <ricardo.lemser@gmail.com>
2024-09-10 14:16:44 +0200

    Initial commit

---
+++ src/main/java/de/ricardo/javaspringservicelesson/JavaSpringServiceLessonApplication.java
@@ -0,0 +1,13 @@
+package de.ricardo.javaspringservicelesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringServiceLessonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringServiceLessonApplication.class, args);
+    }
+
+}
